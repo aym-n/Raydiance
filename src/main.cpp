15df544db3e18b6232b2427bd12759090c211dab
aym-n <ayman_2022bcse004@nitsri.ac.in>
2025-02-25 21:49:31 +0530

    add: sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
+#include <algorithm>
 #include <iostream>
 #include <ostream>
-#include <algorithm>
 
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2.0 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  auto d = b * b - 4 * a * c;
+  return d >= 0;
+}
+
 color ray_color(const ray &r)
 {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    return color(1, 0, 0);
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -33,7 +46,6 @@ int main()
 
   auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
   auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
 
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
