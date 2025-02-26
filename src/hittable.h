c1454e35575957ff86f3def3f51bc3582b9da1ee
aym-n <ayman_2022bcse004@nitsri.ac.in>
2025-02-26 13:55:43 +0530

    add: face determination

--- src/hittable.h
+++ src/hittable.h
@@ -1,7 +1,7 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"]
+#include "ray.h"
 
 class hit_record
 {
@@ -9,6 +9,13 @@ public:
   point3 p;
   vec3 normal;
   double t;
+  bool front_face;
+
+  void set_face_normal(const ray &r, const vec3 &outward_normal)
+  {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
 };
 
 class hittable
