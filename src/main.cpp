417a47552c0b29e430944ab635454a0aeef85c18
aym-n <ayman_2022bcse004@nitsri.ac.in>
2025-02-27 10:33:52 +0530

    add: interval class

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <algorithm>
 #include <iostream>
-#include <memory>
 #include <ostream>
 
 #include "color.h"
 #include "constants.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "interval.h"
 #include "ray.h"
 #include "sphere.h"
 #include "vec3.h"
@@ -28,7 +28,7 @@ double hit_sphere(const point3 &center, double radius, const ray &r)
 color ray_color(const ray &r, const hittable &world)
 {
   hit_record rec;
-  if (world.hit(r, 0, infinity, rec))
+  if (world.hit(r, interval(0, infinity), rec))
     return 0.5 * (rec.normal + color(1, 1, 1));
 
   vec3 unit_direction = unit_vector(r.direction());
--- src/sphere.h
+++ src/sphere.h
@@ -4,6 +4,7 @@
 #include <cmath>
 
 #include "hittable.h"
+#include "interval.h"
 #include "vec3.h"
 
 class sphere : public hittable
@@ -11,7 +12,7 @@ class sphere : public hittable
 public:
   sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-  bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+  bool hit(const ray &r, interval ray_t, hit_record &rec) const override
   {
     vec3 oc = center - r.origin();
     auto a = r.direction().length_squared();
@@ -24,10 +25,10 @@ public:
 
     auto sqrtd = std::sqrt(d);
     auto root = (h - sqrtd) / a;
-    if (root <= ray_tmin || ray_tmax <= root)
+    if (!ray_t.surrounds(root))
     {
       root = (h + sqrtd) / a;
-      if (root <= ray_tmin || ray_tmax <= root)
+      if (!ray_t.surrounds(root))
         return false;
     }
 
